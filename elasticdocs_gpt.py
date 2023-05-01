f8ef9f7e87be0389a2be3d8013ed8c8ebddbf06f
Adam Sheather <adam.sheather@autonomation.co>
2023-05-01 14:02:30 +1000

    last formatting issue I hope

--- elasticdocs_gpt.py
+++ elasticdocs_gpt.py
@@ -114,5 +114,5 @@ if submit_button:
     if negResponse in answer:
         st.write(f"ChatGPT: {answer.strip()}")
     else:
-        title_name = "{resp} Dashboard"
-        st.write(f"ChatGPT: {answer.strip()}\n\nDashboard: [title_name](https://snapshot.kb.us-east-2.aws.elastic-cloud.com:9243/app/dashboards#/view/{url}?_g=(filters:!(),refreshInterval:(pause:!t,value:60000),time:(from:now-90d%2Fd,to:now)))")
+        title_name = f"{resp} Dashboard"
+        st.write(f"ChatGPT: {answer.strip()}\n\nDashboard: [{title_name}](https://snapshot.kb.us-east-2.aws.elastic-cloud.com:9243/app/dashboards#/view/{url}?_g=(filters:!(),refreshInterval:(pause:!t,value:60000),time:(from:now-90d%2Fd,to:now)))")
